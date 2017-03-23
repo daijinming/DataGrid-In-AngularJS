d2e5c90ef0633bed3e529085edd6b539458fa2b9
daijinming <daijinming@live.cn>
2017-03-23 17:59:06 +0800

    数据表格、服务端分页

---
+++ DataGrid-In-AngularJS/HomeModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace DataGrid_In_AngularJS
+{
+    public class HomeModule:NancyModule
+    {
+        public HomeModule()
+        {
+            Get[@"/"] = _ => {
+
+                return this.View["Home.html"];
+            };
+
+            this.Post[@"/API/DataGrid"] = _ => {
+
+                PostItem item = this.Bind<PostItem>();
+
+                var pagesize = item.pagesize;
+                var pageno = item.pageno;
+
+                return Response.AsFile("Content/data.txt", "application/json"); ;
+            };
+        }
+    }
+
+    public class PostItem
+    {
+        public int pagesize { get; set; }
+        public int pageno { get; set; }
+    }
+}
---
+++ DataGrid-In-AngularJS/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataGrid_In_AngularJS.Startup))]
+namespace DataGrid_In_AngularJS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.UseNancy();
+        }
+    }
+}
