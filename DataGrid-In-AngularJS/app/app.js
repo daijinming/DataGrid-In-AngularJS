adb95f2b58ca281301703408f70ec61785af75f5
daijinming <daijinming@live.cn>
2017-03-24 16:19:04 +0800

    每页数量

--- DataGrid-In-AngularJS/app/app.js
+++ DataGrid-In-AngularJS/app/app.js
@@ -5,7 +5,7 @@ app.controller('listdata',function($http){
 	vm.users = []; //declare an empty array
 	vm.pageno = 1; // initialize page no to 1
 	vm.total_count = 0;
-	vm.pagesize = 10; //this could be a dynamic value from a drop down
+	vm.itemsPerPage = 10; //this could be a dynamic value from a drop down
 	
 	vm.getData = function(pageno){ // This would fetch the data on page change.
 		//In practice this should be in a factory.
@@ -14,7 +14,7 @@ app.controller('listdata',function($http){
 		$http({
 		    method: 'post',
 		    url: '/API/DataGrid',
-		    data: { pagesize: vm.pagesize, pageno: vm.pageno }
+		    data: { pagesize: vm.itemsPerPage, pageno: vm.pageno }
 		}).success(function (response) {
 
 			vm.users = response.data;  //ajax request to fetch data into vm.data
