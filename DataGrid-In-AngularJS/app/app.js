e6cd2d419d54aabef81873ffe22b62b70823d405
daijinming <daijinming@live.cn>
2017-03-24 16:48:36 +0800

    当前页字段

--- DataGrid-In-AngularJS/app/app.js
+++ DataGrid-In-AngularJS/app/app.js
@@ -1,25 +1,29 @@
 var app = angular.module('angularTable', ['angularUtils.directives.dirPagination']);
 
 app.controller('listdata',function($http){
-	var vm = this;
-	vm.users = []; //declare an empty array
-	vm.pageno = 1; // initialize page no to 1
-	vm.total_count = 0;
-	vm.itemsPerPage = 10; //this could be a dynamic value from a drop down
+	var self = this;
 	
-	vm.getData = function(pageno){ // This would fetch the data on page change.
-		//In practice this should be in a factory.
-		vm.users = [];
-	    //$http.get("http://code.ciphertrick.com/api/list/page/"+vm.itemsPerPage+"/"+pageno).success(function(response){ 
+	self.users = []; //初始化空数组
+	self.pageno = 1; // 当前页默认为 1
+	self.total_count = 0;
+	self.itemsPerPage = 10; //可以绑定到下拉框列表
+	
+	self.getData = function(pageno){ 
+		
+		self.users = [];
+		self.pageno = pageno;
+	   
 		$http({
 		    method: 'post',
 		    url: '/API/DataGrid',
-		    data: { pagesize: vm.itemsPerPage, pageno: vm.pageno }
+		    data: { pagesize: self.itemsPerPage, pageno: self.pageno }
 		}).success(function (response) {
 
-			vm.users = response.data;  //ajax request to fetch data into vm.data
-			vm.total_count = response.total_count;
+			self.users = response.data;  
+			self.total_count = response.total_count;
 		});
 	};
-	vm.getData(vm.pageno); // Call the function to fetch initial data on page load.
+	
+	self.getData(self.pageno); 
+	
 });
